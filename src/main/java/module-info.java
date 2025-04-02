a1e5e1658c68483290fd0675c2bea2a9ac4d8fd7
Ivan Chaban <chabaiva@student.cvut.cz>
2025-04-02 12:25:02 +0100

    Initial commit

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module cz.cvut.fel.pjv.alchemist_quest {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens cz.cvut.fel.pjv.alchemist_quest to javafx.fxml;
-    exports cz.cvut.fel.pjv.alchemist_quest;
-}
